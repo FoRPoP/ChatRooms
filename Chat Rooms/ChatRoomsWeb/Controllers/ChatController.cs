138003fa4671d9bc4bfa6ee121b0b494f6fac35a
Ognjen Popovic <ogpopovic@microsoft.com>
2024-07-30 15:35:19 +0200

    feat: Implement partial ChatRoom functionality
    Implements the creation/deletion of Chat Rooms, as well as API calls for getting the data of Chat Rooms.

---
+++ Chat Rooms/ChatRoomsWeb/Controllers/ChatController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Interfaces;
+using Interfaces.Helpers;
+using Microsoft.ServiceFabric.Services.Client;
+
+namespace ChatRoomsWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : Controller
+    {
+        private Uri _serviceUri;
+
+        public ChatController()
+        {
+            _serviceUri = new Uri("fabric:/Chat_Rooms/ChatService");
+        }
+
+        [HttpGet]
+        [Route("GetChatRooms")]
+        public async Task<Dictionary<string, ChatData>> GetChatRooms()
+        {
+            IChatService chatService = GetChatService();
+            return await chatService.GetChatRooms();
+        }
+
+        [HttpGet]
+        [Route("GetChatRoom")]
+        public async Task<ChatData?> GetChatRoom(string chatRoomId)
+        {
+            IChatService chatService = GetChatService();
+            return await chatService.GetChatRoom(chatRoomId);
+        }
+
+        [HttpPost]
+        [Route("CreateChatRoom")]
+        public async Task<bool> CreateChatRoom(string name, string description)
+        {
+            IChatService chatService = GetChatService();
+            return await chatService.CreateChatRoom(name, description);
+        }
+
+        [HttpPost]
+        [Route("JoinChatRoom")]
+        public async Task<Chat> JoinChatRoom(string chatRoomId, string userId)
+        {
+            IChatService chatService = GetChatService();
+            return await chatService.JoinChatRoom(chatRoomId, userId);
+        }
+
+        [HttpPost]
+        [Route("SendMessage")]
+        public async Task<bool> SendMessage(string chatRoomId, string userId, Message message)
+        {
+            IChatService chatService = GetChatService();
+            return await chatService.SendMessage(chatRoomId, userId, message);
+        }
+
+        [HttpPost]
+        [Route("LeaveChatRoom")]
+        public async Task<bool> LeaveChatRoom(string chatRoomId, string userId)
+        {
+            IChatService chatService = GetChatService();
+            return await chatService.LeaveChatRoom(chatRoomId, userId);
+        }
+
+        [HttpDelete]
+        [Route("DeleteChatRoom")]
+        public async Task<bool> DeleteChatRoom(string chatRoomId)
+        {
+            IChatService chatService = GetChatService();
+            return await chatService.DeleteChatRoom(chatRoomId);
+        }
+
+        private IChatService GetChatService()
+        {
+            return ServiceProxy.Create<IChatService>(_serviceUri, new ServicePartitionKey(1));
+        }
+    }
+}
--- Chat Rooms/ChatService/ChatService.cs
+++ Chat Rooms/ChatService/ChatService.cs
@@ -1,27 +1,119 @@
-using System;
-using System.Collections.Generic;
-using System.Fabric;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 using Microsoft.ServiceFabric.Data;
+using Interfaces;
+using Interfaces.Helpers;
+using Microsoft.ServiceFabric.Data.Collections;
+using System.Fabric;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 
 namespace ChatService
 {
     /// <summary>
     /// The FabricRuntime creates an instance of this class for each service type instance.
     /// </summary>
-    internal sealed class ChatService : StatefulService
+    internal sealed class ChatService : StatefulService, IChatService
     {
         public ChatService(StatefulServiceContext context)
             : base(context)
         { }
+
+        public async Task<Dictionary<string, ChatData>> GetChatRooms()
+        {
+            IReliableDictionary<string, Chat> _chatRooms = await StateManager.GetOrAddAsync<IReliableDictionary<string, Chat>>("chatRooms");
+
+            Dictionary<string, ChatData> chats = new();
+            using (ITransaction tx = StateManager.CreateTransaction())
+            {
+                var enumerable = await _chatRooms.CreateEnumerableAsync(tx);
+                var enumerator = enumerable.GetAsyncEnumerator();
+
+                while (await enumerator.MoveNextAsync(CancellationToken.None))
+                {
+                    var kvp = enumerator.Current;
+                    chats.Add(kvp.Key, kvp.Value.ChatData);
+                }
+            }
+
+            return chats;
+        }
+
+        public async Task<ChatData?> GetChatRoom(string chatRoomId)
+        {
+            IReliableDictionary<string, Chat> _chatRooms = await StateManager.GetOrAddAsync<IReliableDictionary<string, Chat>>("chatRooms");
+
+            ChatData? chatData;
+            using (ITransaction tx = StateManager.CreateTransaction())
+            {
+                ConditionalValue<Chat> chat = await _chatRooms.TryGetValueAsync(tx, chatRoomId);
+                if (chat.HasValue)
+                {
+                    chatData = chat.Value.ChatData;
+                }
+                else
+                {
+                    chatData = null;
+                }
+                await tx.CommitAsync();
+            }
+
+            return chatData;
+        }
+
+        public async Task<bool> CreateChatRoom(string name, string description)
+        {
+            IReliableDictionary<string, Chat> _chatRooms = await StateManager.GetOrAddAsync<IReliableDictionary<string, Chat>>("chatRooms");
+            IReliableDictionary<string, int> _IDs = await StateManager.GetOrAddAsync<IReliableDictionary<string, int>>("IDs");
+
+            bool result;
+            using (ITransaction tx = StateManager.CreateTransaction())
+            {
+                int nextChatRoomId = await _IDs.AddOrUpdateAsync(tx, "ChatRoomIDs", 0, (_, value) => value+1);
+                ChatData chatData = new(nextChatRoomId.ToString(), name, description, "test");
+                Chat chat = new(chatData);
+
+                result = await _chatRooms.TryAddAsync(tx, chatData.ID, chat);
+                await tx.CommitAsync();
+            }
+
+            return result;
+        }
+
+        public Task<Chat> JoinChatRoom(string chatRoomId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LeaveChatRoom(string chatRoomId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SendMessage(string chatRoomId, string userId, Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteChatRoom(string chatId)
+        {
+            IReliableDictionary<string, Chat> _chatRooms = await StateManager.GetOrAddAsync<IReliableDictionary<string, Chat>>("chatRooms");
+
+            using (ITransaction tx = StateManager.CreateTransaction())
+            {
+                bool exists = await _chatRooms.ContainsKeyAsync(tx, chatId, LockMode.Update);
+                if (exists)
+                {
+                    await _chatRooms.TryRemoveAsync(tx, chatId);
+                    await tx.CommitAsync();
+                    return true;
+                }
+                else
+                {
+                    await tx.CommitAsync();
+                    return false;
+                }
+            }
+        }
 
         /// <summary>
         /// Optional override to create listeners (like tcp, http) for this service instance.
@@ -29,39 +121,7 @@ namespace ChatService
         /// <returns>The collection of listeners.</returns>
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
-            return new ServiceReplicaListener[]
-            {
-                new ServiceReplicaListener(serviceContext =>
-                    new KestrelCommunicationListener(serviceContext, (url, listener) =>
-                    {
-                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
-
-                        var builder = WebApplication.CreateBuilder();
-
-                        builder.Services
-                                    .AddSingleton<StatefulServiceContext>(serviceContext)
-                                    .AddSingleton<IReliableStateManager>(this.StateManager);
-                        builder.WebHost
-                                    .UseKestrel()
-                                    .UseContentRoot(Directory.GetCurrentDirectory())
-                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.UseUniqueServiceUrl)
-                                    .UseUrls(url);
-                        builder.Services.AddControllers();
-                        builder.Services.AddEndpointsApiExplorer();
-                        builder.Services.AddSwaggerGen();
-                        var app = builder.Build();
-                        if (app.Environment.IsDevelopment())
-                        {
-                        app.UseSwagger();
-                        app.UseSwaggerUI();
-                        }
-                        app.UseAuthorization();
-                        app.MapControllers();
-                        
-                        return app;
-
-                    }))
-            };
+            return this.CreateServiceRemotingReplicaListeners();
         }
     }
 }
