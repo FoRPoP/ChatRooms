f19fec4c746c09cf3ed93a1f851e9075be7ef972
Ognjen Popovic <ogpopovic@microsoft.com>
2024-08-13 13:44:05 +0200

    feat: Increase timeout

--- chat_rooms_client/src/axiosConfig.ts
+++ chat_rooms_client/src/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:9090',
-    timeout: 10000
+    timeout: 1000000
 });
 
 axiosInstance.interceptors.request.use(config => {
